639e41b33241c9ecbb071beee9de9be4aca193fe
Kit Ho Chan <117521882+tyeckh@users.noreply.github.com>
2025-06-29 11:31:17 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const LimSimiQuiz = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [titleClickCount, setTitleClickCount] = useState(0);
   const [isValidated, setIsValidated] = useState(false);
-  const validTesterCodes = ["BATOCKS", "KYSUCKS"];
+  const validTesterCodes = ["BATOCKS", "KYSUCKS","HARLOTESTING"];
 
   const handleTesterCodeSubmit = (e) => {
     e.preventDefault();
